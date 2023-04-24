99585c94059e0ddbe328b9cf154b55469615bb03
Patrick Wehbe <50799614+patrickwehbe@users.noreply.github.com>
2023-04-24 21:12:13 +0300

    Revert "Login added"
    This reverts commit 2b9b171f642a7204476d7dbc18a4faa8f2977780.

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,13 @@ import "./App.css";
 import Home from "./views/home.view";
 import Value from "./views/value.view";
 import Application from "./views/application.view";
-import Login from "./views/Login";
 
 function App() {
-  return (
+	return (
 		<div className="app">
 			<Router>
 				<Routes>
-					<Route path="/" element={<Login />} />
+					<Route path="/" element={<Home />} />
 					<Route path="/value" element={<Value />} />
 					<Route path="/applications" element={<Application />} />
 				</Routes>
