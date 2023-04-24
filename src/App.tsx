7d02b08d7af749e59a429b3f9f37cdc78766471c
Jad Assaad <jta16@mail.aub.edu>
2023-04-24 16:18:57 +0300

    Login added

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import "./App.css";
 import Home from "./views/home.view";
 import Value from "./views/value.view";
 import Application from "./views/application.view";
+import Login from "./views/Login";
 
 function App() {
-	return (
+  return (
 		<div className="app">
 			<Router>
 				<Routes>
-					<Route path="/" element={<Home />} />
+					<Route path="/" element={<Login />} />
 					<Route path="/value" element={<Value />} />
 					<Route path="/applications" element={<Application />} />
 				</Routes>
