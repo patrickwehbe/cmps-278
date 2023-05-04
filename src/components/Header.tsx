dcb2d8c48090fb4ab8e084691b6233c33386209e
Patrick Wehbe <patrick.wehbe.github@gmail.com>
2023-05-04 11:58:45 +0300

    t

---
+++ src/components/Header.tsx
@@ -0,0 +1,137 @@
+import * as React from "react";
+import { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Box from "@mui/material/Box";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import SearchIcon from "@mui/icons-material/Search";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import IconButton from "@mui/material/IconButton";
+import { styled } from "@mui/material/styles";
+import { SvgIcon, SvgIconTypeMap } from "@mui/material";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
+import { grey } from "@mui/material/colors";
+
+interface HeaderProps {
+	onTabChange: (newValue: string) => void;
+}
+
+const Header: React.FC<HeaderProps> = ({ onTabChange }) => {
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+	const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleMenuClose = () => {
+		setAnchorEl(null);
+	};
+
+	const handleTabChange = (event: React.SyntheticEvent, newValue: string) => {
+		onTabChange(newValue);
+	};
+
+	return (
+		<AppBar position="static" sx={{ bgcolor: "#fff", color: "#000" }}>
+			<Toolbar sx={{ justifyContent: "space-between" }}>
+				<Box display="flex" alignItems="center">
+					<GoogleLogo />
+					<StyledTabs
+						value={false}
+						onChange={handleTabChange}
+						aria-label="header tabs"
+						TabIndicatorProps={{
+							style: {
+								backgroundColor: "#3cba54",
+								height: "3px",
+								borderTopLeftRadius: "3px",
+								borderTopRightRadius: "3px",
+							},
+						}}
+					>
+						<StyledTab label="Games" value={false} />
+						<StyledTab label="Apps" value={false} />
+						<StyledTab label="Movies" value={false} />
+						<StyledTab label="Books" value={false} />
+					</StyledTabs>
+				</Box>
+				<Box display="flex" alignItems="center">
+					<IconButton aria-label="search">
+						<SearchIcon />
+					</IconButton>
+					<IconButton aria-label="help">
+						<HelpOutlineIcon />
+					</IconButton>
+					<IconButton aria-label="avatar" onClick={handleMenuOpen}>
+						<StyledAvatar
+							alt="User Avatar"
+							src="https://picsum.photos/id/237/200/200"
+						/>
+					</IconButton>
+					<StyledMenu
+						anchorEl={anchorEl}
+						open={Boolean(anchorEl)}
+						onClose={handleMenuClose}
+					>
+						<StyledMenuItem onClick={handleMenuClose}>
+							Wishlist
+						</StyledMenuItem>
+						<StyledMenuItem onClick={handleMenuClose}>Profile</StyledMenuItem>
+						<StyledMenuItem onClick={handleMenuClose}>
+							Login/Logout
+						</StyledMenuItem>
+					</StyledMenu>
+				</Box>
+			</Toolbar>
+		</AppBar>
+	);
+};
+
+const GoogleLogo: React.FC = () => <SvgIcon component={GoogleIcon} sx={{ mr: 1 }} />;
+
+const GoogleIcon: OverridableComponent<SvgIconTypeMap<{}, "svg">> = styled("svg")(
+	({ theme }) => ({
+		width: "24px",
+		height: "24px",
+		fill: theme.palette.primary.main,
+	})
+);
+
+const StyledTabs = styled(Tabs)({
+	"& .MuiTab-root": {
+		fontWeight: "bold",
+		textTransform: "none",
+		minWidth: "unset",
+		padding: "6px 16px",
+		margin: "0 6px",
+		"&.Mui-selected": {
+			color: "#3cba54",
+		},
+		"&:hover": {
+			backgroundColor: grey[100],
+		},
+	},
+});
+
+const StyledTab = styled(Tab)({
+	minWidth: "unset",
+});
+
+const StyledAvatar = styled(Avatar)({
+	width: "32px",
+	height: "32px",
+});
+
+const StyledMenu = styled(Menu)({
+	"& .MuiMenu-paper": {
+		boxShadow: "0px 8px 16px rgba(0, 0, 0, 0.1)",
+	},
+});
+
+const StyledMenuItem = styled(MenuItem)({
+	fontWeight: "bold",
+});
+
+export default Header;
--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -25,58 +25,64 @@ const Login = () => {
 		setRightPanelActive(false);
 	};
 	return (
-		<div className="container" id="container" ref={containerRef}>
-			<div className="form-container register-container">
-				<form action="#">
-					<h1>Register</h1>
-					<input type="text" placeholder="Name" />
-					<input type="email" placeholder="Email" />
-					<input type="password" placeholder="Password" />
-					<button>Register</button>
-				</form>
-			</div>
-			<div className="form-container login-container">
-				<form action="#">
-					<h1>Login</h1>
-					<input type="email" placeholder="Email" />
-					<input type="password" placeholder="Password" />
-					<div className="content">
-						<div className="checkbox">
-							<input type="checkbox" name="checkbox" id="checkbox" />
-							<label>Remember me</label>
+		<div className="login">
+			<div className="container" id="container" ref={containerRef}>
+				<div className="form-container register-container">
+					<form action="#">
+						<h1>Register</h1>
+						<input type="text" placeholder="Name" />
+						<input type="email" placeholder="Email" />
+						<input type="password" placeholder="Password" />
+						<button>Register</button>
+					</form>
+				</div>
+				<div className="form-container login-container">
+					<form action="#">
+						<h1>Login</h1>
+						<input type="email" placeholder="Email" />
+						<input type="password" placeholder="Password" />
+						<div className="content">
+							<div className="checkbox">
+								<input type="checkbox" name="checkbox" id="checkbox" />
+								<label>Remember me</label>
+							</div>
+							<div className="pass-link">
+								<a href="PasswordReset">Forgot password?</a>
+							</div>
 						</div>
-						<div className="pass-link">
-							<a href="PasswordReset">Forgot password?</a>
-						</div>
-					</div>
-					<button>Login</button>
-				</form>
-			</div>
+						<button>Login</button>
+					</form>
+				</div>
 
-			<div className="overlay-container">
-				<div className="overlay">
-					<div className="overlay-panel overlay-left">
-						<h1 className="title">Hello friends</h1>
-						<p>If you already have an account, login here!</p>
-						<button className="ghost" id="login" onClick={handleLoginClick}>
-							Login
-							<i className="lni lni-arrow-left login"></i>
-						</button>
-					</div>
-					<div className="overlay-panel overlay-right">
-						<h1 className="title">Start your journey now</h1>
-						<p>
-							If you don't have an account yet, join us and start your
-							journey.{" "}
-						</p>
-						<button
-							className="ghost"
-							id="register"
-							onClick={handleRegisterClick}
-						>
-							Register
-							<i className="lni lni-arrow-right register"></i>
-						</button>
+				<div className="overlay-container">
+					<div className="overlay">
+						<div className="overlay-panel overlay-left">
+							<h1 className="title">Hello friends</h1>
+							<p>If you already have an account, login here!</p>
+							<button
+								className="ghost"
+								id="login"
+								onClick={handleLoginClick}
+							>
+								Login
+								<i className="lni lni-arrow-left login"></i>
+							</button>
+						</div>
+						<div className="overlay-panel overlay-right">
+							<h1 className="title">Start your journey now</h1>
+							<p>
+								If you don't have an account yet, join us and start your
+								journey.{" "}
+							</p>
+							<button
+								className="ghost"
+								id="register"
+								onClick={handleRegisterClick}
+							>
+								Register
+								<i className="lni lni-arrow-right register"></i>
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
