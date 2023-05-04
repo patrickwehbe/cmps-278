cedbd6ee51dca50ef2feac3901796565f3951a59
Patrick Wehbe <patrick.wehbe.github@gmail.com>
2023-05-04 20:12:55 +0300

    fix header and date of card template

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Box from "@mui/material/Box";
@@ -15,6 +15,7 @@ import { styled } from "@mui/material/styles";
 import { SvgIcon, SvgIconTypeMap } from "@mui/material";
 import { OverridableComponent } from "@mui/material/OverridableComponent";
 import { grey } from "@mui/material/colors";
+import { useNavigate } from "react-router-dom";
 
 interface HeaderProps {
 	onTabChange: (newValue: string) => void;
@@ -32,15 +33,44 @@ const Header: React.FC<HeaderProps> = ({ onTabChange }) => {
 	const handleTabChange = (event: React.SyntheticEvent, newValue: string) => {
 		onTabChange(newValue);
 	};
+	const [isScrolled, setIsScrolled] = useState(false);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY > 0) {
+				setIsScrolled(true);
+			} else {
+				setIsScrolled(false);
+			}
+		};
+
+		window.addEventListener("scroll", handleScroll);
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
 
 	return (
 		<AppBar
-			position="static"
-			sx={{ bgcolor: "#fff", color: "#000", marginBottom: "80px" }}
+			position="fixed"
+			sx={{
+				bgcolor: isScrolled ? "white" : "transparent",
+				color: "#000",
+				marginBottom: "100px",
+				padding: "25px 15px",
+				position: "sticky",
+				boxShadow: isScrolled ? "0px 0px 10px rgba(0, 0, 0, 0.2)" : "none",
+				transition: "box-shadow 0.3s ease-in-out",
+			}}
 		>
 			<Toolbar sx={{ justifyContent: "space-between" }}>
 				<Box display="flex" alignItems="center">
-					<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu1PJmT_THldF0n5APcmt9p10utgu6KSw4cH2fQ5Xhpw&s" />
+					<img
+						src="https://logos-world.net/wp-content/uploads/2020/12/Google-Play-Logo.png"
+						style={{ objectFit: "contain", height: "80px" }}
+					/>
 					<StyledTabs
 						value={false}
 						onChange={handleTabChange}
@@ -54,10 +84,26 @@ const Header: React.FC<HeaderProps> = ({ onTabChange }) => {
 							},
 						}}
 					>
-						<StyledTab label="Games" value={false} />
-						<StyledTab label="Apps" value={false} />
-						<StyledTab label="Movies" value={false} />
-						<StyledTab label="Books" value={false} />
+						<StyledTab
+							label="Games"
+							value={false}
+							onClick={() => navigate("/games")}
+						/>
+						<StyledTab
+							label="Apps"
+							value={false}
+							onClick={() => navigate("/applications")}
+						/>
+						<StyledTab
+							label="Movies"
+							value={false}
+							onClick={() => navigate("/movies")}
+						/>
+						<StyledTab
+							label="Books"
+							value={false}
+							onClick={() => navigate("/books")}
+						/>
 					</StyledTabs>
 				</Box>
 				<Box display="flex" alignItems="center">
--- src/components/MovieCardTemplate.tsx
+++ src/components/MovieCardTemplate.tsx
@@ -5,6 +5,11 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
+function formatDate(dateString: string | number | Date) {
+	const date = new Date(dateString);
+	const options = { year: "numeric", month: "long", day: "numeric" };
+	return date.toLocaleDateString("en-US", options as any);
+}
 
 export default function MovieCardTemplate({
 	movie_id,
@@ -16,11 +21,11 @@ export default function MovieCardTemplate({
 	date_released,
 }: any) {
 	return (
-		<Card sx={{ maxWidth: 345, minWidth: 345 }} key={movie_id}>
+		<Card sx={{ maxWidth: 345, minWidth: 345, minHeight: 345 }} key={movie_id}>
 			<CardActionArea>
 				<CardMedia
 					component="img"
-					height="140"
+					height="440"
 					image={movie_image}
 					alt={movie_name}
 				/>
@@ -38,7 +43,7 @@ export default function MovieCardTemplate({
 						{movie_rating}
 					</Typography>
 					<Typography variant="body2" color="text.secondary">
-						{date_released}
+						{formatDate(date_released)}
 					</Typography>
 				</CardContent>
 			</CardActionArea>
