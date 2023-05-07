448a489d4c711dce121db570962ce4863150779c
Patrick Wehbe <patrick.wehbe.github@gmail.com>
2023-05-07 22:03:26 +0300

    fixes

--- src/components/AppReview.tsx
+++ src/components/AppReview.tsx
@@ -21,6 +21,8 @@ import { useAddAppReplyMutation, useGetAllAppRepliesQuery } from "../api/apprepl
 import { any } from "prop-types";
 import { useGetAllUsersQuery } from "../api/user.api";
 import { useGetAllAppReviewsQuery } from "../api/appreview.api";
+import { useSelector } from "react-redux";
+import { selectUser } from "../redux/auth";
 
 export default function AppReview({
 	app_review_id,
@@ -32,7 +34,6 @@ export default function AppReview({
 	user_image,
 	user_username,
 }: any) {
-<<<<<<< HEAD
 	const [likes, setLikes] = useState(num_of_likes);
 	const [liked, setLiked] = useState(false);
 	const [replyDialogOpen, setReplyDialogOpen] = useState(false);
@@ -44,6 +45,7 @@ export default function AppReview({
 	const [addAppReply] = useAddAppReplyMutation();
 	const [showReplies, setShowReplies] = useState(false);
 	const [replyList, setReplyList] = useState(replies);
+	const user = useSelector(selectUser);
 
 	useEffect(() => {
 		setReplyList(replies);
@@ -83,7 +85,7 @@ export default function AppReview({
 
 		const newAppReply = {
 			content: replyText,
-			user_fid: randomUserFid,
+			user_fid: user.user_id,
 			app_review_fid: app_review_id,
 		};
 
@@ -101,7 +103,10 @@ export default function AppReview({
 				<Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
 					<div className="reviewDetails">
 						<Stack direction="row" alignItems="center" spacing={2}>
-							<Avatar src={user_image} />
+							<Avatar
+								src={user_image}
+								alt={user?.user_username.charAt(0)}
+							/>
 							<Typography
 								variant="body1"
 								component="div"
@@ -181,7 +186,10 @@ export default function AppReview({
 								sx={{ marginLeft: "10%", marginTop: "2%" }}
 							>
 								<Stack direction="row" alignItems="center" spacing={2}>
-									<Avatar src={user?.user_image || ""} />
+									<Avatar
+										src={user?.user_image}
+										alt={user?.user_username.charAt(0)}
+									/>
 									<Typography variant="body1" component="div">
 										{user?.user_username || "Unknown"}
 									</Typography>
@@ -232,202 +240,4 @@ export default function AppReview({
 			</Dialog>
 		</Card>
 	);
-=======
-  const [likes, setLikes] = useState(num_of_likes);
-  const [liked, setLiked] = useState(false);
-  const [replyDialogOpen, setReplyDialogOpen] = useState(false);
-  const [replyText, setReplyText] = useState("");
-  const { data: allReplies } = useGetAllAppRepliesQuery({});
-  const { data: allReviews } = useGetAllAppReviewsQuery({});
-  const replies = allReplies?.filter((reply: any) => reply.app_review_fid === app_review_id) || [];
-  const [addAppReply] = useAddAppReplyMutation();
-  const [showReplies, setShowReplies] = useState(false);
-  const [replyList, setReplyList] = useState(replies);
-  
-
-
-  useEffect(() => {
-		setReplyList(replies);
-	}, [addAppReply]);
-
-
-  const { data: allUsers } = useGetAllUsersQuery({});
-
-  const findUser = (userFid: number) => {
-    return allUsers?.find((user: any) => user.user_id === userFid) || null;
-  };
-
-  const toggleReplies = () => {
-    setShowReplies(!showReplies);
-  };
-
-
-  const handleLike = () => {
-    if (liked) {
-      setLikes(likes - 1);
-    } else {
-      setLikes(likes + 1);
-    }
-    setLiked(!liked);
-  };
-
-  const handleReplyDialogOpen = () => {
-    setReplyDialogOpen(true);
-  };
-
-  const handleReplyDialogClose = () => {
-    setReplyDialogOpen(false);
-  };
-
-  const handleReplySubmit = async () => {
-    console.log("Submitted reply:", replyText);
-
-    const randomUserFid = Math.floor(Math.random() * 6) + 1;
-
-    const newAppReply = {
-      content: replyText,
-      user_fid: randomUserFid,
-      app_review_fid: app_review_id,
-    };
-
-    await addAppReply(newAppReply);
-
-    setReplyList([...replyList, newAppReply]);
-    setReplyText("");
-    handleReplyDialogClose();
-  };
-  console.log(replies);
-
-  return (
-    
-    <Card sx={{ maxWidth: 1000, boxShadow: "none" }} key={app_review_id}>
-      <CardContent>
-        <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
-          <div className="reviewDetails">
-            <Stack direction="row" alignItems="center" spacing={2}>
-              <Avatar src={user_image} />
-              <Typography
-                variant="body1"
-                component="div"
-                className="appreview_username"
-              >
-                {user_username}
-              </Typography>
-            </Stack>
-            <Stack direction="row" alignItems="center" spacing={1}>
-              <Typography
-                variant="body2"
-                color="text.secondary"
-                className="appreview_rating"
-                sx={{ marginLeft: "5%" }}
-              >
-                {review_rating}
-              </Typography>
-              <Rating value={review_rating} precision={0.1} readOnly />
-            </Stack>
-            <Typography
-              variant="body2"
-              color="text.secondary"
-              sx={{ marginTop: "16px", fontWeight: "bold" }}
-            >
-              {content}
-            </Typography>
-            <Box display="flex" justifyContent="space-between" alignItems="center">
-              <Button
-                variant="contained"
-                color="primary"
-                sx={{
-                  textTransform: "none",
-                }}
-                onClick={handleReplyDialogOpen}
-              >
-                Reply
-              </Button>
-              <Box display="flex" alignItems="center">
-                <IconButton
-                  color={liked ? "primary" : "default"}
-                  onClick={handleLike}
-                >
-                  <ThumbUpIcon />
-                </IconButton>
-                <Typography
-                  variant="body2"
-                  color="text.secondary"
-                  className="appreview_likes"
-                >
-                  {likes}
-                </Typography>
-              </Box>
-            </Box>
-          </div>
-        </Stack>
-        </CardContent>
-
-<Button
-  onClick={toggleReplies}
-  color="primary"
-  sx={{ marginLeft: "10%", marginBottom: "16px" }}
->
-  {showReplies ? "Hide Replies" : "Show Replies"}
-</Button>
-
-{showReplies && (
-  <Card sx={{ maxWidth: 1000, boxShadow: "none" }} key={app_review_id}>
-    {replyList.map((reply: any) => {
-      const user = findUser(reply.user_fid);
-      return (
-        <Box key={reply.id} sx={{ marginLeft: "10%", marginTop: "2%" }}>
-          <Stack direction="row" alignItems="center" spacing={2}>
-            <Avatar src={user?.user_image || ""} />
-            <Typography variant="body1" component="div">
-              {user?.user_username || "Unknown"}
-            </Typography>
-          </Stack>
-          <Typography
-            variant="body2"
-            color="text.secondary"
-            sx={{ marginLeft: "56px", marginTop: "1%" }}
-          >
-            {reply.content}
-          </Typography>
-        </Box>
-      );
-    })}
-  </Card>
-)}
-
-      <Dialog
-        open={replyDialogOpen}
-        onClose={handleReplyDialogClose}
-        aria-labelledby="reply-dialog-title"
-        maxWidth="md"
-        fullWidth
-      >
-        <DialogTitle id="reply-dialog-title">Reply to Review</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="reply"
-            label="Your Reply"
-            type="text"
-            fullWidth
-            multiline
-            rows={4}
-            value={replyText}
-            onChange={(e) => setReplyText(e.target.value)}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleReplyDialogClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleReplySubmit} color="primary">
-            Submit
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </Card>
-  );
->>>>>>> c303d75210d0d5a2eecc12152e3aff72e8da118a
 }
--- src/components/BooksCardTemplate.tsx
+++ src/components/BooksCardTemplate.tsx
@@ -6,43 +6,42 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
 import "./BooksCardTemplate.css";
-import StarIcon from '@mui/icons-material/Star'
-
+import StarIcon from "@mui/icons-material/Star";
 
 export default function BookCardTemplate({
-  book_id,
-  book_name,
-  book_cover,
-  book_rating,
-  book_price,
-  book_author,
+	book_id,
+	book_name,
+	book_cover,
+	book_rating,
+	book_price,
+	book_author,
 }: any) {
-  return (
-    <Card sx={{ maxWidth: 345 }} key={book_id}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="300"
-          image={book_cover}
-          alt={book_name}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {book_name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {book_author}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {book_price}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {book_rating}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-      </CardActions>
-    </Card>
-  );
+	return (
+		<Card sx={{ maxWidth: 345 }} key={book_id}>
+			<CardActionArea>
+				<CardMedia
+					component="img"
+					height="300"
+					image={book_cover}
+					alt={book_name}
+				/>
+				<CardContent>
+					<Typography gutterBottom variant="h5" component="div">
+						{book_name}
+					</Typography>
+					<Typography variant="body2" color="text.secondary">
+						{book_author}
+					</Typography>
+					<Typography variant="body2" color="text.secondary">
+						{book_price == 0 ? "Free" : book_price + "$"}
+					</Typography>
+					<Typography variant="body2" color="text.secondary">
+						{book_rating}{" "}
+						<StarIcon sx={{ fontSize: "14px", color: "#ffc107" }} />
+					</Typography>
+				</CardContent>
+			</CardActionArea>
+			<CardActions></CardActions>
+		</Card>
+	);
 }
